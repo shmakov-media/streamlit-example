61dac2078ce3303ee7241d2063691ee38199f67f
shmakov-media <118623575+shmakov-media@users.noreply.github.com>
2022-11-19 20:08:43 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,7 @@ new_sting = profile__sting[0:8]+'-'+profile__sting[8:12]+'-'+profile__sting[12:1
 ####
 profile_data = requests.get(f'https://yappy.media/api/profile/{new_sting}').json()
 # Данные о профиле
-st.text('Никнейм:',profile_data['user']['nickname'])
-
+st.write(profile_data['nickname'])
 ####
 data = requests.get(f'https://yappy.media/api/video-list/{new_sting}').json()
 st.dataframe(pd.DataFrame(pd.DataFrame(data['data']['results'])))
