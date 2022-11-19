8ad016bde2c87c5dc1051352ed4b79635fa3250e
shmakov-media <118623575+shmakov-media@users.noreply.github.com>
2022-11-19 19:59:13 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,8 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 
 profile__sting = st.text_input('Укажите ID-аккаунта')
-new_sting = profile__sting[0:8]+'-'+profile__sting[9:13]+'-'+profile__sting[14:18]+'-'+profile__sting[19:23]+'-'+profile__sting[24:32]
+new_sting = profile__sting[0:8]+'-'+profile__sting[8:12]+'-'+profile__sting[12:16]+'-'+profile__sting[16:20]+'-'+profile__sting[20:32]
+
 
 data = requests.get(f'https://yappy.media/api/video-list/{new_sting}').json()
 st.dataframe(pd.DataFrame(pd.DataFrame(data['data']['results'])))
